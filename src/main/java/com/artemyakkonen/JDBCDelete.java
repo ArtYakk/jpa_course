4b5a33008c930289d7361bb833e1dee0b4e363e9
Artem <iakonien@gmail.com>
2025-05-22 13:53:11 +0300

    delete с помощью jdbc, preparedStatement.executeUpdate()

---
+++ src/main/java/com/artemyakkonen/JDBCDelete.java
@@ -0,0 +1,41 @@
+package com.artemyakkonen;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCDelete {
+    static final String DB_URL = "jdbc:postgresql://109.120.155.136:5432/test_db";
+    static final String USER = "jpauser";
+    static final String PWD = "jpapwd";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PWD);
+
+            PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM students WHERE surname = ?"
+            );
+            statement.setString(1, "Tregulov");
+           int deletedRows =  statement.executeUpdate();
+
+           System.out.println("deleted rows: " + deletedRows);
+           statement.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
