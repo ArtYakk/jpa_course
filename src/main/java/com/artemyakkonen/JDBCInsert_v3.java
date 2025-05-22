d7b02f7936335570b082f9f7fb07fa346ef7d347
Artem <iakonien@gmail.com>
2025-05-22 14:10:06 +0300

    insert в базу данных с возвратом id сущности, после запроса пишем PreparedStatement.RETURN_GENERATED_KEYS, потом из statement.getGeneratedKeys получаем ключ, с помощью .next находим значение. Кстати statement.executeUpdate возвращает affectedRows

---
+++ src/main/java/com/artemyakkonen/JDBCInsert_v3.java
@@ -0,0 +1,56 @@
+package com.artemyakkonen;
+
+import java.sql.*;
+
+public class JDBCInsert_v3 {
+    static final String DB_URL = "jdbc:postgresql://109.120.155.136:5432/test_db";
+    static final String USER = "jpauser";
+    static final String PWD = "jpapwd";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Student student = new Student("Isaac", "Sharp", 9.8);
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PWD);
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO students(name, surname, avg_grade) values (?, ?, ?)",
+                    PreparedStatement.RETURN_GENERATED_KEYS
+            );
+            statement.setString(1, student.getName());
+            statement.setString(2, student.getSurname());
+            statement.setDouble(3, student.getAvgGrade());
+
+           int affectedRows = statement.executeUpdate();
+           if(affectedRows == 0){
+               throw new SQLException("Failed to add Student to DB");
+           }
+
+           ResultSet generatedKeys = statement.getGeneratedKeys();
+           if(generatedKeys.next()){
+               student.setId(generatedKeys.getLong(1));
+           }
+           else{
+               throw new SQLException("Failed to create student ID");
+           }
+
+            System.out.println(student);
+
+            statement.close();
+            generatedKeys.close();
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
