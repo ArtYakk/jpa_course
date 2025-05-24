55313d60a11a14dbb3bf80f88f1e4ecca2b7db46
iakon <iakonien@gmail.com>
2025-05-24 20:16:50 +0300

    Получили ищ базы студента с заданным id с помощью метода .find(), предоставленного менеджером сущностей, в аргументах указали тип класса, который ожидаем получить из базы (Ентити класс), и нужный нам id, закрыли менеджер сущностей, закрыли фабрику менеджеров сущностей.

---
+++ src/main/java/com/artemyakkonen/jpa_crud/Find_ex.java
@@ -0,0 +1,35 @@
+package com.artemyakkonen.jpa_crud;
+
+import com.artemyakkonen.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class Find_ex {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory("jpa-course");
+        EntityManager entityManager = factory.createEntityManager();
+
+
+            Student student = null;
+
+            try {
+                student = entityManager.find(Student.class, 7);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            finally {
+                if(entityManager != null){
+                    entityManager.close();
+                    factory.close();
+                }
+                System.out.println(student);
+            }
+
+
+
+    }
+}
