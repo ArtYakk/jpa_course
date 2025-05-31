0c089971a5652e6b62bfea18d8b638ce9db6095e
iakon <iakonien@gmail.com>
2025-05-31 17:48:48 +0300

    Изучили метод entityManager.detach(entity), который убирает сущность из контекста, после чего коммит не будет заливать в базу никакие изменения, однако если методом entityManager.persist(entity) попытаться сохранить в базу сущность, а потом сделать detach, то сущность все же сохранится в базу данных, так как это происходить сразу при применении метода persist, соответственно коммит все сохранит

---
+++ src/main/java/com/artemyakkonen/persistence_context/jpa_methods/DetachEx2.java
@@ -0,0 +1,39 @@
+package com.artemyakkonen.persistence_context.jpa_methods;
+
+import com.artemyakkonen.persistence_context.entity.Subject;
+import com.artemyakkonen.persistence_context.entity.Teacher;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class DetachEx2 {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa-course");
+        EntityManager entityManager = factory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try{
+            transaction.begin();
+
+            Teacher teacher = new Teacher("Carina", "Demis", Subject.HISTORY, false);
+            entityManager.persist(teacher);
+            entityManager.detach(teacher);
+
+            transaction.commit();
+        }
+        catch(Exception e){
+            if(transaction != null){
+                System.out.println("!!!ROLLBACK!!!");
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally{
+            if(entityManager != null){
+                entityManager.close();
+                factory.close();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/artemyakkonen/persistence_context/jpa_methods/DetachEx3.java
@@ -0,0 +1,41 @@
+package com.artemyakkonen.persistence_context.jpa_methods;
+
+import com.artemyakkonen.persistence_context.entity.Subject;
+import com.artemyakkonen.persistence_context.entity.Teacher;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class DetachEx3 {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa-course");
+        EntityManager entityManager = factory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try{
+            transaction.begin();
+
+            Teacher teacher = entityManager.find(Teacher.class, 4);
+
+            entityManager.detach(teacher);
+
+            entityManager.remove(teacher);
+
+            transaction.commit();
+        }
+        catch(Exception e){
+            if(transaction != null){
+                System.out.println("!!!ROLLBACK!!!");
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally{
+            if(entityManager != null){
+                entityManager.close();
+                factory.close();
+            }
+        }
+    }
+}
