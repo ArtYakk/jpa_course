8362d9322c9c75cd515f437e9c4a67cad8dda725
iakon <iakonien@gmail.com>
2025-05-31 18:03:37 +0300

    Рассмотрели метод entityManager.refresh(entity), который актуализирует информацию о сущности из базы данных, на случай если ее кто-то изменил, по сути делается новый селект, так что все что мы делали с этой сущностью до refresh, не имеет значения, так как подгрузится новая версия в персистенс контекст

---
+++ src/main/java/com/artemyakkonen/persistence_context/jpa_methods/RefreshEx2.java
@@ -0,0 +1,42 @@
+package com.artemyakkonen.persistence_context.jpa_methods;
+
+import com.artemyakkonen.persistence_context.entity.Subject;
+import com.artemyakkonen.persistence_context.entity.Teacher;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class RefreshEx2 {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa-course");
+        EntityManager entityManager = factory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try{
+            transaction.begin();
+
+            Teacher teacher = entityManager.find(Teacher.class, 1);
+            teacher.setSubject(Subject.MATHS);
+            entityManager.flush();
+
+            teacher.setProfessor(false);
+            entityManager.refresh(teacher);
+
+            transaction.commit();
+        }
+        catch(Exception e){
+            if(transaction != null){
+                System.out.println("!!!ROLLBACK!!!");
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally{
+            if(entityManager != null){
+                entityManager.close();
+                factory.close();
+            }
+        }
+    }
+}
