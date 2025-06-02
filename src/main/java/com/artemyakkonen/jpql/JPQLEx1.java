761c7a5c17e808be9ff218df876a34979ea1986c
Artem <iakonien@gmail.com>
2025-06-02 09:51:22 +0300

    Начали изучение JPQL, создаем Query через entityManager.createQuery(запрос), дальше получаем результат в лист через query.getResultList. Если при создании запроса после "запрос" через запятую указать класс, получится TypedQuery<Тип>

--- src/main/java/com/artemyakkonen/jpql/JPQLEx1.java
+++ src/main/java/com/artemyakkonen/jpql/JPQLEx1.java
@@ -18,13 +18,36 @@ public class JPQLEx1 {
         try {
             transaction.begin();
 
-            // ALL STUDENTS
-            // select * from students;
+//            ALL STUDENTS
+//            select * from students;
 
-            Query query = entityManager.createQuery("select s from Student s");
-            List<Student> students = query.getResultList();
+//            Query query = entityManager.createQuery("select s from Student s");
+//            List<Student> students = query.getResultList();
+//            System.out.println(students);
+
+//            TypedQuery<Student> query = entityManager.createQuery("select s from Student s", Student.class);
+//            List<Student> students = query.getResultList();
+//            System.out.println(students);
+
+//            List<Student> students = entityManager.createQuery("select s from Student s").getResultList();
+//            System.out.println(students);
+
+//            List<Student> list = entityManager.createQuery("from Student").getResultList();
+//******************************************************************************************************************
+//            ALL STUDENTS WITH NAME LEO
+//            List<Student> students = entityManager.createQuery("select s from Student s " +
+//                    "where s.name = 'Leo'").getResultList();
+//            System.out.println(students);
+//******************************************************************************************************************
+//            ALL STUDENTS WITH AVG GRADE > 8.5
+//            List<Student> students = entityManager.createQuery("select s from Student s " +
+//                    "where s.avgGrade > 8.5").getResultList();
+//            System.out.println(students);
+//******************************************************************************************************************
+//            ALL STUDENTS WITH AVG BETWEEN 7 AND 8
+            List<Student> students = entityManager.createQuery("select s from Student s " +
+                    "where s.avgGrade between 7 and 8").getResultList();
             System.out.println(students);
-
 
             transaction.commit();
         }catch (Exception e){
