c3567646102f667ff2732186a1fa5611f1d688a6
iakon <iakonien@gmail.com>
2025-06-03 22:39:28 +0300

    Получили студента из базы данных. (get).  Без транзакции get не работает как find в jpa

---
+++ src/main/java/com/artemyakkonen/hibernate/HibernateEx2.java
@@ -0,0 +1,41 @@
+package com.artemyakkonen.hibernate;
+
+import com.artemyakkonen.hibernate.entity.Student;
+import com.artemyakkonen.util.MyColor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HibernateEx2 {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(HibernateEx2.class);
+        SessionFactory sessionFactory  = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        try{
+            transaction.begin();
+
+            Student student = session.get(Student.class, 2);
+            System.out.println(student);
+
+
+            transaction.commit();
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            logger.error(MyColor.color("Ошибка в транзакции: " + e));
+        }finally {
+            if(session != null){
+                session.close();
+            }
+            sessionFactory.close();
+        }
+    }
+}
