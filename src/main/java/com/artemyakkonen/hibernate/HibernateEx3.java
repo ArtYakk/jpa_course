ca2408d0d13a55fc3bc7ad0abaa57c8210800b34
Artem <iakonien@gmail.com>
2025-06-04 10:50:27 +0300

    Получили студента из базы и изменили, точно также как и в jpa. Сначала получили студента, он пришел в состоянии persistent(managed) вместе со своим снэпшотом, затем сеттером назначили поле, при коммите automatic dirty checking функция persistent context обратила внимание на то что снэпшот != ентити, залила изменения в базу данных

---
+++ src/main/java/com/artemyakkonen/hibernate/HibernateEx3.java
@@ -0,0 +1,43 @@
+package com.artemyakkonen.hibernate;
+
+import com.artemyakkonen.hibernate.entity.Student;
+import com.artemyakkonen.util.MyColor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HibernateEx3 {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(HibernateEx3.class);
+        SessionFactory sessionFactory  = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        try{
+            transaction.begin();
+
+            Student student = session.get(Student.class, 2);
+            student.setAvgGrade(9.0);
+            System.out.println("Average grade was updated");
+            System.out.println(student);
+
+
+            transaction.commit();
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            logger.error(MyColor.color("Ошибка в транзакции: " + e));
+        }finally {
+            if(session != null){
+                session.close();
+            }
+            sessionFactory.close();
+        }
+    }
+}
